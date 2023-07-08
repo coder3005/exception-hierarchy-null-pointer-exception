37c50ba903cb03d5b2d3c4681ad5798aa86f0024
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-08 12:16:57 +0530

    Create NPE.java

---
+++ NPE.java
@@ -0,0 +1,17 @@
+/* NullPointerException */
+class NPE
+{
+    public static void main(String[] args) {
+        
+        String str=null;
+
+        try
+        {
+            System.out.println(str.toUpperCase());
+        }
+        catch(NullPointerException n)
+        {
+            System.out.println("null can't be casted");
+        }
+    }
+}
